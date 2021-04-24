0bb353c2da4937060a9af7829a29885f9b4f73a8
jolayemi_local <ajolayemi82@gmail.com>
2021-04-24 15:54:01 +0200

    Defined check_indent_error module

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 import os
+import re
+
 
 MAX_LINE_LEN = 79
 
 ERRORS_DICT = {'Long Line': 'S001',
                'Indentation': 'S002',
-               'Statement Semicolon': 'S003',
+               'Semicolon': 'S003',
                'Inline Comments': 'S004',
-               'TODO Found': 'S005',
-               'Many blank lines': 'S006'}
-
+               'TODO': 'S005',
+               'Blank lines': 'S006'}
+indent_pattern = r'^ +'
 file = input()
 
 valid_file = os.path.exists(file)
@@ -21,6 +23,26 @@ class Analyser:
     def __init__(self, file_to_analyse: str = file):
         self.file = file_to_analyse
         self.invalid_file = os.path.exists(self.file)
+        self.all_errors = {}
+
+    def check_indent_error(self):
+        current_line = self.file_reader()
+        if current_line:
+            try:
+                while True:
+                    c_line, c_line_num = next(current_line)
+                    line_str = f'Line {c_line_num}'
+                    indent_num = len(c_line) - len(c_line.lstrip())
+                    if indent_num % 4 > 0 and len(c_line) > 1:
+                        if line_str in self.all_errors:
+                            self.all_errors[line_str].append([ERRORS_DICT.get('Indentation'),
+                                                              c_line_num])
+                        else:
+                            self.all_errors[line_str] = []
+                            self.all_errors[line_str].append([ERRORS_DICT.get('Indentation'),
+                                                              c_line_num])
+            except StopIteration:
+                pass
 
     def file_reader(self) -> tuple:
         """ Loops through the provided file yielding each line and it's corresponding
@@ -32,14 +54,16 @@ class Analyser:
         try:
             with open(self.file) as file_:
                 for line in file_:
-                    yield line.strip(), c_line
+                    yield line, c_line
                     c_line += 1
         except FileNotFoundError:
             return ()
 
 
 def main():
-    pass
+    s = Analyser()
+    s.check_indent_error()
+    print(s.all_errors)
 
 
 if __name__ == '__main__':
